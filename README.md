f922760c6e36a6db40d226dc6bc73a2dc3cb9142
Kevin <kevin.hantzen@gmail.com>
2019-05-18 11:24:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Le Calcul Shadok
+-----------------
+
+- https://www.youtube.com/watch?v=lP9PaDs2xgQ
+- https://www.maths-et-tiques.fr/index.php/detentes/cours-de-calcul-shadoks
---
+++ shadok.hs
@@ -0,0 +1,42 @@
+import Test.Hspec
+
+type Decimal = Int
+type ShadokNumber = String
+
+shadok :: Decimal -> ShadokNumber
+shadok 0  = "GA"
+shadok 1  = "BU"
+shadok 2  = "ZO"
+shadok 3  = "MEU"
+shadok x
+    | x `mod` 4 == 0 = shadok ( x `div` 4 ) ++ "GA"
+    | otherwise      = shadok ( x `div` 4 ) ++ shadok ( x `mod` 4 )
+
+
+main = hspec $ do
+
+    describe "Shadok numeric base" $ do
+        it "0, 1, 2, 3 should be GA BU ZO MEU" $ do
+             shadok 0 `shouldBe` "GA"
+             shadok 1 `shouldBe` "BU"
+             shadok 2 `shouldBe` "ZO"
+             shadok 3 `shouldBe` "MEU"
+
+
+    describe "Multiple of four" $ do
+        it "With only one GA" $ do
+            shadok 4  `shouldBe` "BUGA"
+            shadok 8  `shouldBe` "ZOGA"
+            shadok 12 `shouldBe` "MEUGA"
+
+        it "With multiple GA" $ do
+            shadok 16 `shouldBe` "BUGAGA"
+
+    describe "Any number which is not a multiple of four" $ do
+        it "two shadoks verb number" $ do
+            shadok 7  `shouldBe` "BUMEU"
+            shadok 15 `shouldBe` "MEUMEU"
+            shadok 5  `shouldBe` "BUBU"
+
+        it "three shadoks verb number" $ do
+            shadok 19 `shouldBe` "BUGAMEU"
